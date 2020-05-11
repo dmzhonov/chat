c1b9a92db7324a8eafffde02c2a8dafb3819fe2a
Sergey Zhzhonov <zhzhonovsergey@gmail.com>
2020-05-11 18:41:08 +0300

    Добавил верстку

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="/portfolio/spisok-knig/jquery.js"></script>
+	<script type="text/javascript" src="script.js"></script>
 	<title>Анонимный чат v0.1</title>
 </head>
-<body>
-
+<body class="bg-dark">
+	<div id="chatwindow">
+		<div id="header">
+			<h5>Омерзительная сука</h5>
+			<p>Был(о) в сети примерно никогда</p>
+		</div>
+		<form mothod="GET">
+			<input type="text" name="message" size="74" placeholder="Введите сообщение...">
+			<button type="submit">
+			    <image id="send" src="pics/button.png">
+			</button>
+		</form>
+	</div>
 </body>
 </html>
